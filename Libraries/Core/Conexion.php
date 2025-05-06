43bcfb8337b1ac55216f377a17f6ecab007f096b
enrique <enrique-vimac@hotmail.com>
2025-05-05 21:34:44 -0500

    first

---
+++ Libraries/Core/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+class Conexion {
+    
+    private $conection;
+
+    public function __construct()
+    {
+        $connectionString = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";" . DB_CHARSET;
+        try {
+            $this->conection = new \PDO($connectionString, DB_USER, DB_PASSWORD);
+            $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Error:" . $e->getMessage();
+        }
+    }
+
+    public function connect() {
+        return $this->conection;
+    }
+}
---
+++ Views/Nosotros/nosotros.php
@@ -0,0 +1,91 @@
+<?php
+headerTienda($data);
+?>
+<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('<?= media() ?>tienda/images/abc.jpg');">
+    <h2 class="ltext-105 cl0 txt-center">
+        Acerca de Nosotros
+    </h2>
+</section>
+
+
+<!-- Content page -->
+<section class="bg0 p-t-75 p-b-120">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 col-lg-8">
+                <div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+                    <h3 class="mtext-111 cl2 p-b-16">
+                        Acerca de:
+                    </h3>
+
+                    <p class="stext-113 cl6 p-b-26 text-justify">
+                    CompuSoftNet es una empresa especializada en brindar soluciones informáticas integrales para hogares, pequeñas empresas y negocios en crecimiento. Nos enfocamos en ofrecer un servicio técnico confiable, rápido y personalizado, comprometidos con la calidad y la satisfacción de nuestros clientes.
+
+                    </p>
+
+                    <p class="stext-113 cl6 p-b-26 text-justify">
+                    Contamos con un equipo capacitado en reparación y mantenimiento de computadoras, eliminación de virus, formateo e instalación de software, así como asesoría en tecnología. Nuestro objetivo es ayudarte a mantener tus equipos funcionando de manera óptima, para que puedas concentrarte en lo que realmente importa: tu trabajo, tus estudios o tu negocio.
+                    </p>
+
+                    
+                </div>
+            </div>
+
+            <div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+                <div class="how-bor1 ">
+                    <div class="hov-img0">
+                        <img src="<?= media() ?>tienda/images/mac.jpg" alt="IMG">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="bg6 flex-c-m flex-w size-302 m-t-73 p-tb-15">
+            <span class="stext-107 cl6 p-lr-25 text-justify">
+            En CompuSoftNet creemos que la tecnología debe estar al servicio de las personas, por eso trabajamos cada día para ofrecer soluciones accesibles, eficientes y con atención cercana.
+            </span>
+
+        </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+                <div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+                    <h3 class="mtext-111 cl2 p-b-16">
+                        Nuestra Mision:
+                    </h3>
+
+                    <p class="stext-113 cl6 p-b-26 text-justify">
+                    Ofrecer a los clientes y trabajadores de la institución un servicio de calidad y pertinencia en servicios técnicos, desarrollo de sistemas, comercialización y distribución de productos informáticos, que contribuyan a la formación y aprovechamiento mediante el uso de las tecnologías de la información e innovación, aplicando la ética de trabajo, creatividad y respeto a la naturaleza.
+
+                    </p>
+                    <h3 class="mtext-111 cl2 p-b-16">
+                        Nuestra Vision:
+                    </h3>
+
+                    <p class="stext-113 cl6 p-b-26 text-justify">
+                    Posesionarnos como una empresa líder en el mercado con un servicio eficiente dedicado a satisfacer las necesidades de nuestros clientes, basados en la mejora continua, con sistemas de información y soporte técnico calificado, para constituirse como una herramienta esencial que represente una ventaja de aprendizaje y desarrollo para los usuarios.
+
+                    </p>
+
+
+                </div>
+            </div>
+
+            <div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+                <div class="how-bor2">
+                    <div class="hov-img0">
+                        <img src="<?= media() ?>tienda/images/teclado.jpg" alt="IMG">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+//}
+
+footerTienda($data);
+?>
